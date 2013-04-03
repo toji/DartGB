94f4d38299a50635b3ab7491680f4e888ea484e2
Maksym Taran <maksym.taran@gmail.com>
2013-04-03 13:48:36 -0700

    Added simple ROM loading.

--- web/rom.dart
+++ web/rom.dart
@@ -1,4 +1,9 @@
 part of dartgb;
 
 class ROM {
+  Uint8List data = new Uint8List(0x8000); // Limited to 32k right now.
+  
+  ROM(String filename) {
+    HttpRequest.getString(filename).then((s) => data.addAll(s.codeUnits));
+  }
 }
