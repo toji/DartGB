770441c256fc2e388734af05a97d448f9403b1c1
Brandon Jones <bajones@chromium.org>
2013-04-04 12:24:42 -0700

    More robust background handling

--- web/lcd.dart
+++ web/lcd.dart
@@ -70,29 +70,59 @@ class LCD {
     present();
   }
   
-  // 0xFF40  LCD and GPU control Read/write
+  // 0xFF40  LCD and GPU control Read/write memory.LCDC
   // 0xFF47  Background palette  Write only
   
   int get ScrollX => memory.SCX;
   int get ScrollY => memory.SCY;
   int get ScanLine => memory.LY;
   
+  bool get BackgroundEnabled => (memory.LCDC & 0x01) == 1;
+  bool get SpritesEnabled => (memory.LCDC & 0x02) == 1;
+  int get SpriteHeight => (memory.LCDC & 0x04) == 0 ? 8 : 16;
+  int get BackgroundTileSet => (memory.LCDC & 0x08) == 0 ? 0 : 1;
+  int get BackgroundOffset => (memory.LCDC & 0x10) == 0 ? 0x1C00 : 0x1800;
+  bool get WindowEnabled => (memory.LCDC & 0x20) == 1;
+  int get WindowTiles => (memory.LCDC & 0x40) == 0 ? 0 : 1;
+  bool get DisplayEnabled => (memory.LCDC & 0x80) == 1;
+  
   int Pallet(int index) {
     return (memory.BGP >> (index * 2)) & 0x3;
   }
+  
+  int BackgroundTile(int index) {
+    int tile = memory.R(BackgroundOffset + index);
+    return BackgroundTileSet == 1 && tile < 128 ? tile += 256 : tile;
+  }
 
   Uint8Array _scanline = new Uint8Array(512);
+  
+  void SetScanline(int offset, int value) {
+    int scanlineOffset = offset * 2;
+    if(value == 0) {
+      _scanline[scanlineOffset] = 255;
+      _scanline[scanlineOffset+1] = 0;
+    } else if(value == 1) {
+      _scanline[scanlineOffset] = 192;
+      _scanline[scanlineOffset+1] = 255;
+    } else if(value == 2) {
+      _scanline[scanlineOffset] = 96;
+      _scanline[scanlineOffset+1] = 255;
+    } else {
+      _scanline[scanlineOffset] = 0;
+      _scanline[scanlineOffset+1] = 255;
+    }
+  }
+  
   void renderScan() {
     int y = ScanLine;
     int scanlineOffset = 0;
     
     //print("Rendering Scanline $ScanLine");
-    
-    int backgroundMapOffset = 0x9800; // for looping through the tile maps
-    
+
     for(int x = 0; x < 32; ++x) {
       int backgroundIndex = x + ((y / 8).toInt() * 32);
-      int tileIndex = memory.R(backgroundMapOffset + backgroundIndex);
+      int tileIndex = BackgroundTile(backgroundIndex);
       int tileOffset = 0x8000 + (tileIndex * 16) + ((y % 8) * 2);
       
       int rowLow = memory.R(tileOffset);
@@ -100,22 +130,8 @@ class LCD {
       for(int i = 0; i < 8; ++i) {
         int tileValue = ((rowLow >> i) & 0x01) + ((rowHigh >> i) & 0x02);
         tileValue = Pallet(tileValue);
-        //int tileValue = (i + j) % 4; // For Great Debugging!
         
-        // TODO: Pallet lookup
-        if(tileValue == 0) {
-          _scanline[scanlineOffset++] = 255;
-          _scanline[scanlineOffset++] = 0;
-        } else if(tileValue == 1) {
-          _scanline[scanlineOffset++] = 192;
-          _scanline[scanlineOffset++] = 255;
-        } else if(tileValue == 2) {
-          _scanline[scanlineOffset++] = 96;
-          _scanline[scanlineOffset++] = 255;
-        } else {
-          _scanline[scanlineOffset++] = 0;
-          _scanline[scanlineOffset++] = 255;
-        }
+        SetScanline(x + i, tileValue);
       }
     }
     
