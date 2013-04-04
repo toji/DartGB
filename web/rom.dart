5f070fc2ddd6d96a7efea8a842f8f963451e883b
Dominic Hamon <dma+github@stripysock.com>
2013-04-03 21:58:49 -0700

    More flexible HttpRequest

--- web/rom.dart
+++ web/rom.dart
@@ -4,10 +4,17 @@ class ROM {
   Uint8List data = new Uint8List(0x8000); // Limited to 32k right now.
   
   ROM(String filename) {
-    HttpRequest.getString(filename).then((s) => data.addAll(s.codeUnits))
-                                   .catchError((error) {
-                                      print(error.toString());
-                                      assert(false);
-                                   });
+    // TODO: change the request so it doesn't return a string which
+    // is NULL-terminated.
+    HttpRequest.request(filename)
+        .then((req) {
+            assert(req.response.byteLength == data.lengthInBytes);
+            var view = new Uint8Array.fromBuffer(req.response);
+            data.setRange(0, data.length, view.asList());
+        })
+        .catchError((error) {
+            print(error.toString());
+            assert(false);
+        });
   }
 }
